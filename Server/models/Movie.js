cdfc18579254e983227941146868a6fd6615c1a0
Kristian Lalov <lalov.kr@gmail.com>
2023-01-21 21:48:19 +0200

    added some movieschema requirements

--- Server/models/Movie.js
+++ Server/models/Movie.js
@@ -1,6 +1,37 @@
 const { model, Schema, Types } = require('mongoose');
 
+const IMAGE_REGEX = /^https?:\/\/.+$/i;
 
-const movieSchema = new Schema({ 
-    
-})
+const movieSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: [2, 'Title should be at least 2 charakters long!'],
+  },
+  year: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: (value) => {
+        value <= 1900 && value >= 2030;
+      },
+      message: 'Year cannot be lower then 1900 and higher then 2030!',
+    },
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => {
+        IMAGE_REGEX.text(value);
+      },
+      message: 'Invalid Image URl!',
+    },
+  },
+  description: {
+    type: String,
+    required: true,
+    minLength: [10, 'Description should be at least 10 charakters long!'],
+  },
+  
+});
---
+++ Server/models/User.js
@@ -0,0 +1,5 @@
+const {model, Types, Schema} = require('mongoose')
+
+const userModel = new Schema({ 
+    
+})
