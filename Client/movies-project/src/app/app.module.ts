cccef610044c1ce7563af9577fba88aa0dbaedd2
Kristian Lalov <lalov.kr@gmail.com>
2023-01-28 23:50:47 +0200

    added footer component

--- Client/movies-project/src/app/app.module.ts
+++ Client/movies-project/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Client/movies-project/src/app/core/header/header.component.ts
+++ Client/movies-project/src/app/core/header/header.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
+
 export class HeaderComponent {
 
 }
