8db54f0b2ba16837ca7de8ee1a3222d9e68af90e
Kristian Lalov <lalov.kr@gmail.com>
2023-02-03 23:41:11 +0200

    added main component

--- Client/movies-project/src/app/core/core.module.ts
+++ Client/movies-project/src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     CommonModule
