e003acf1c994a66d81a17e017589403e52f139de
Kristian Lalov <lalov.kr@gmail.com>
2023-02-21 22:45:33 +0200

    refactor client

--- Client/movies-project/src/app/app.module.ts
+++ Client/movies-project/src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    CoreModule,
-     AuthModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent] 
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
