917205e59d061c91cf21761ccad5e848af2937a2
Kristian Lalov <lalov.kr@gmail.com>
2023-02-22 02:12:56 +0200

    added some header css

--- Client/movies-project/src/app/core/core.module.ts
+++ Client/movies-project/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from '../material/material.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
