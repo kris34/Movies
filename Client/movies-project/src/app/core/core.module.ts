343a34cba1a594621230091d3d30ef369b0f167d
Kristian Lalov <lalov.kr@gmail.com>
2023-03-04 17:59:12 +0200

    added footer

--- Client/movies-project/src/app/core/core.module.ts
+++ Client/movies-project/src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from '../material/material.module';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule, MaterialModule, RouterModule,],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
