dfdb95470ed1457fdbd2727727e0a3521ae30c11
Kristian Lalov <lalov.kr@gmail.com>
2023-03-17 17:27:02 +0200

    added dynamic homepage

--- Client/movies-project/src/app/auth/auth.module.ts
+++ Client/movies-project/src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [RegisterComponent],
+  declarations: [RegisterComponent, LoginComponent],
   imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
