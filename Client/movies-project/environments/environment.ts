6816b0036a8451c4c0ab88aed6160691d66c36a2
Kristian Lalov <lalov.kr@gmail.com>
2023-03-19 23:55:00 +0200

    created register function

--- Client/movies-project/environments/environment.ts
+++ Client/movies-project/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiURL: 'http://localhost:3000/api',
+  apiURL: 'http://localhost:3001',
 };
--- Client/movies-project/src/app/auth/auth.service.ts
+++ Client/movies-project/src/app/auth/auth.service.ts
@@ -1,10 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { tap } from 'rxjs';
+import { IUser } from '../shared/interfaces/user';
+
+const apiUrl = environment.apiURL;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  user!: IUser | null;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
+  register(userData: {}) {
+    return this.http.post<IUser>(`${apiUrl}/auth/register`, userData).pipe(tap((response) => {
+      if (!response._id) { return }
+    }))
+  }
 }
