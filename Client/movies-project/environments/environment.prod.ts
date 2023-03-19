720aa1617646266a57a12ace79cfbd143af54c1e
Kristian Lalov <lalov.kr@gmail.com>
2023-03-19 23:35:10 +0200

    added environments to the project

---
+++ Client/movies-project/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiURL: 'http://localhost:3000/api'
+  };
+  
--- Client/movies-project/src/app/auth/auth.service.ts
+++ Client/movies-project/src/app/auth/auth.service.ts
@@ -4,9 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  get isLoggedIn() {
-    return false;
-  }
 
   constructor() { }
+
 }
