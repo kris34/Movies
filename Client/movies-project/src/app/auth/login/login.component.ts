42c59addbe49995ab923b28b3ae8faa1d0b71ef0
Kristian Lalov <lalov.kr@gmail.com>
2023-03-24 23:46:28 +0200

    created login form

--- Client/movies-project/src/app/auth/login/login.component.ts
+++ Client/movies-project/src/app/auth/login/login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+
+  
+  form = this.fb.group({ 
+    
+  })
+
+  constructor(private auth: AuthService, private fb: FormBuilder) {}
+
+  loginHandler(){ 
+
+  }
 
 }
