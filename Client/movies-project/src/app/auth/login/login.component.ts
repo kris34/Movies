7119eeeabb8f43dd828550ad36563d601f069c90
Kristian Lalov <lalov.kr@gmail.com>
2023-03-24 23:53:14 +0200

    Removed some not needed login form stuff

--- Client/movies-project/src/app/auth/login/login.component.ts
+++ Client/movies-project/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -9,15 +9,16 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent {
 
-  
-  form = this.fb.group({ 
-    
-  })
+
+  form = this.fb.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
 
   constructor(private auth: AuthService, private fb: FormBuilder) {}
 
-  loginHandler(){ 
+  loginHandler() {
 
+    
   }
-
 }
