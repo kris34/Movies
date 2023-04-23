6a224839a6c57e6103b36eb1164d876dedec3b25
Kristian Lalov <lalov.kr@gmail.com>
2023-04-23 13:49:04 +0300

    finished edit profile page

--- Client/movies-project/src/app/auth/edit-profile/edit-profile.component.ts
+++ Client/movies-project/src/app/auth/edit-profile/edit-profile.component.ts
@@ -1,10 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.css']
+  styleUrls: ['./edit-profile.component.css'],
 })
 export class EditProfileComponent {
+  error: string | undefined = undefined;
 
+  form = this.fb.group({
+    
+  })
+
+  constructor(private auth: AuthService, private fb: FormBuilder) {}
+
+  editHandler() {
+    return;
+  }
 }
