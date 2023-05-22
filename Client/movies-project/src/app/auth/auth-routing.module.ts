9ceeb42e34707598f174ddc88f26b30894446032
Kristian Lalov <lalov.kr@gmail.com>
2023-05-22 23:57:34 +0300

    finalised home page for now

--- Client/movies-project/src/app/auth/auth-routing.module.ts
+++ Client/movies-project/src/app/auth/auth-routing.module.ts
@@ -15,8 +15,7 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent,
     canActivate:[AuthGuardService]
-  },
-  {
+  },  {
     path: 'profile',
     component: ProfileComponent,
   },
