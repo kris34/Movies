7e60571f3fe296c46cdfebe93568b053ba5fbb76
Kristian Lalov <lalov.kr@gmail.com>
2023-07-15 16:40:18 +0300

    render commit

--- Server/index.js
+++ Server/index.js
@@ -4,9 +4,9 @@ const siteController = require('./controllers/siteController');
 const authController = require('./controllers/authController');
 const cors = require('./middlewares/cors');
 const session = require('./middlewares/session');
-const port = '3001';
+const port = '3000';
 const cookieParser = require('cookie-parser');
-const DBConnectionString = 'mongodb://localhost:27017/movies2';
+const DBConnectionString = 'mongodb+srv://kris:kasi4kata9@movies.hwvqfuh.mongodb.net/?retryWrites=true&w=majority';
 
 start();
 
--- Server/middlewares/cors.js
+++ Server/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'https://64b29f7b3071ba0f6f8785c8--glittery-buttercream-62041e.netlify.app/');
   res.header('Access-Control-Allow-Credentials', true);
   res.setHeader(
     'Access-Control-Allow-Methods',
