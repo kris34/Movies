a79f389c4d33c34ec67ce70fe1c2481f21702437
Kristian Lalov <lalov.kr@gmail.com>
2023-07-24 19:49:10 +0300

    added new logic to homepage

--- Client/movies-project/src/app/core/home/home.component.ts
+++ Client/movies-project/src/app/core/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { ApiService } from 'src/app/api.service';
+import { IMovie } from 'src/app/shared/interfaces/movie';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,15 @@ import { ApiService } from 'src/app/api.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  movies$ = this.api.loadMovies();
+  movies: IMovie[] | null = null;
 
   constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.loadMovies().subscribe({
+      next: (v) => {
+        this.movies = v.slice(-4);
+      },
+    });
+  }
 }
