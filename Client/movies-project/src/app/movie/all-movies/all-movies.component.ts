9902a63a23997385e62a7522222081b505f79460
Kristian Lalov <lalov.kr@gmail.com>
2023-08-06 02:53:18 +0300

    added no movies message to user-movie-list

--- Client/movies-project/src/app/movie/all-movies/all-movies.component.ts
+++ Client/movies-project/src/app/movie/all-movies/all-movies.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 
 @Component({
@@ -10,7 +11,9 @@ export class AllMoviesComponent {
 
   movies$ = this.api.loadMovies()
 
-  constructor(private api: ApiService) {
+  constructor(private api: ApiService, private router: Router) {
   }
+
+
 
 }
--- Client/movies-project/src/app/user/list/list.component.ts
+++ Client/movies-project/src/app/user/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 import { MovieService } from 'src/app/movie/movie.service';
@@ -8,15 +8,29 @@ import { MovieService } from 'src/app/movie/movie.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
   list$ = this.api.loadUserList();
+  noMovies: boolean = false
+
 
   constructor(private api: ApiService, private movieService: MovieService, private router: Router) { }
+  ngOnInit(): void {
+    this.list$.subscribe({
+      next: (v) => {
+        if (v.length < 1) {
+          this.noMovies = true
+        }
+      }
+    })
+  }
+
+
 
   deleteMovie(id: string) {
     this.movieService.deleteMovie(id).subscribe({
       next: (v) => {
         this.list$ = this.api.loadUserList();
+
       }
     })
   }
